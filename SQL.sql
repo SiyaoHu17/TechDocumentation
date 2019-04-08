316685789577633c7f3f43e009a3aa17bf55312f
Siyao Hu <31359054+SiyaoHu17@users.noreply.github.com>
2019-04-08 16:45:50 -0700

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,10 @@
+
+GetDate()
+regular expression
+https://docs.oracle.com/cd/B14117_01/appdev.101/b10795/adfns_re.htm
+listunagg
+https://stackoverflow.com/questions/13189575/listunagg-function
+join....on...and....
+rank() over (partition by..order by….desc ):rank inside group
+create temporary table a select * from b
+SELECT TIMESTAMP("2017-07-23")
