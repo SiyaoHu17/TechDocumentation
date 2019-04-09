69f1d080bb6b6052d22705700f981ff8d4c4fe41
Siyao Hu <31359054+SiyaoHu17@users.noreply.github.com>
2019-04-09 11:04:41 -0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 username ="username"
 password = "password"
-connectString="CNUDC029DF-scan.cnb.corp.net:1521/mdmtst_etl "
+connectString="server:port/servicename"
 mode=ora.SYSDBA
 connection = ora.connect(username, password, connectString)
 data=pd.read_sql("SELECT * FROM VVIJAYAK.ACH_IN_OUT", con=connection)
