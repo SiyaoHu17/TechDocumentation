0fdddeca19c6464b685b03bd5c83dfdd5d5d30b2
Siyao Hu <31359054+SiyaoHu17@users.noreply.github.com>
2020-01-14 18:18:59 -0800

    Update Python.py

--- Python.py
+++ Python.py
@@ -14,3 +14,33 @@ connectString="server:port/servicename"
 mode=ora.SYSDBA
 connection = ora.connect(username, password, connectString)
 data=pd.read_sql("SELECT * FROM VVIJAYAK.ACH_IN_OUT", con=connection)
+
+
+Basics:
+use len(df. index) for getting the number of rows, and len(df. columns) for the column count. 
+df.shape[0]
+df.sort_values(by='col1', ascending=False, na_position='first')
+pd.DataFrame({'A' : []})
+pd.DataFrame({'A' : [np.nan]})
+pd.DataFrame(data, index=index, columns=columns)
+df.reset_index(drop=True)
+
+For loop:
+for i in range(len(colors)):
+    print(colors[i])
+    
+    
+    
+Visualization:
+ https://pandas.pydata.org/pandas-docs/version/0.15.0/visualization.html
+  https://pandas.pydata.org/pandas-docs/version/0.23.4/generated/pandas.DataFrame.plot.html
+  http://jonathansoma.com/lede/data-studio/matplotlib/adding-grid-lines-to-a-matplotlib-chart/
+  
+In [58]: ax = df.plot(kind='scatter', x='a', y='b',
+   ....:              color='DarkBlue', label='Group 1');
+   ....: 
+
+In [59]: df.plot(kind='scatter', x='c', y='d',
+   ....:         color='DarkGreen', label='Group 2', ax=ax);
+  
+https://stackoverflow.com/questions/40811592/pyplot-scatterdataframe-vs-dataframe-plotkind-scatter
