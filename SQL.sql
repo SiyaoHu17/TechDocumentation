495663e139e911b60221881db001deda62d50855
Siyao Hu <31359054+SiyaoHu17@users.noreply.github.com>
2020-01-22 16:34:32 -0800

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -8,3 +8,31 @@ join....on...and....
 rank() over (partition by..order by….desc ):rank inside group
 create temporary table a select * from b
 SELECT TIMESTAMP("2017-07-23")
+
+
+
+
+MySQL Syntax:
+
+SELECT column_name(s) FROM table_name WHERE condition LIMIT number;
+
+Oracle Syntax:
+
+SELECT column_name(s) FROM table_name WHERE ROWNUM <= number;
+
+
+
+
+import cx_Oracle as ora
+
+
+import pandas as pd
+
+
+username ="SiyaoHu"
+password = "xxxxx"
+connectString="CNUDC029DF-SCAN.CNB.CORP.NET:1521/CITYACCESS_DEV"
+mode=ora.SYSDBA
+connection = ora.connect(username, password, connectString)
+
+data=pd.read_sql("SELECT * FROM ETLACH.STG_ACH_TRANSACTION LIMIT 10", con=connection)
