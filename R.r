eead13643e74a20a96d6fa328f72c8a4f850a2d7
Siyao Hu <31359054+SiyaoHu17@users.noreply.github.com>
2020-02-24 10:34:14 -0800

    Update R.r

--- R.r
+++ R.r
@@ -1,1 +1,16 @@
+#logical operators
+https://stat.ethz.ch/R-manual/R-devel/library/base/html/Logic.html
+! x
+x & y
+x && y
+x | y
+x || y
+xor(x, y)
 
+isTRUE (x)
+isFALSE(x)
+
+
+
+#explore data
+mtcars[order(mpg, -cyl),]
