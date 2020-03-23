0fb112e25be6688d8f43522f78f79081c03bd07b
Siyao Hu <31359054+SiyaoHu17@users.noreply.github.com>
2020-03-23 10:33:01 -0700

    Update R.r

--- R.r
+++ R.r
@@ -1,3 +1,10 @@
+#package resources
+#https://www.rdocumentation.org/
+#https://rdrr.io/
+
+#training
+#https://rstudio.com/resources/training/
+
 #logical operators
 https://stat.ethz.ch/R-manual/R-devel/library/base/html/Logic.html
 ! x
@@ -9,13 +16,38 @@ xor(x, y)
 
 isTRUE (x)
 isFALSE(x)
+is.null(x)
 
-
+#distinct rows
+distinct()  #dplyr 
+unique()
 
 #explore data
 mtcars[order(mpg, -cyl),]
 
+#cleaning
+d$a <- factor(d$a)
+
 
 #shiny
+#training
+#https://shiny.rstudio.com/tutorial/
+
 #https://www.rdocumentation.org/packages/shinyWidgets/versions/0.5.0/topics/updatePickerInput
 #http://shinyapps.dreamrs.fr/shinyWidgets/
+
+#layout
+#https://shiny.rstudio.com/articles/layout-guide.html
+
+#widget
+##shinyWidget
+##https://dreamrs.github.io/shinyWidgets/
+prettyCheckbox()
+searchInput()
+
+##html
+#https://shiny.rstudio.com/articles/tag-glossary.html
+#https://shiny.rstudio.com/tutorial/written-tutorial/lesson2/
+#
+##app great example
+##https://shiny.rstudio.com/gallery/animal-tracking.html
